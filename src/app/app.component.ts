9c44c7b728b4131ac2937031f294a531feb4dedf
Akash-Patel-95 <akashpatel4980@gmail.com>
2021-07-18 11:28:09 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './services/user.service';
+import { ToastrService } from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
   title = 'random-card';
+  user: any;
+
+  constructor(
+    private userservice: UserService, 
+    private toaster: ToastrService,
+
+
+    ){}
+ 
+  ngOnInit(){
+    this.userservice.getUser().subscribe(
+      (user: any) => {
+        console.log(user);
+        this.user = user.results[0];
+      },
+      (err) => {
+        this.toaster.error(err.status, "Oops!");
+      },
+    );
+  }
+
+   // Function to reload the application
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+//http
+import { HttpClientModule } from "@angular/common/http";
+//toaster
+import{ BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ToastrModule } from "ngx-toastr";
+//fontawesome
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
